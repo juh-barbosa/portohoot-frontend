d08f45e0ecd584c1c7dcbd3e00c211c8d79c80e6
juh-barbosa <jujufreitas193@gmail.com>
2024-06-09 14:55:03 -0300

    update

--- src/pages/Timer/Timer.tsx
+++ src/pages/Timer/Timer.tsx
@@ -1,20 +1,38 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import io from 'socket.io-client';
 
 const Timer = () => {
   const [remainingTime, setRemainingTime] = useState(null);
   const [duration, setDuration] = useState('');
-  const socket = io('https://portohoot-backend.vercel.app/'); // Certifique-se de usar a URL correta do seu servidor
+  const socket = io('http://localhost:8000'); // Certifique-se de usar a URL correta do seu servidor
+
+  // Função debounce
+  const debounce = (func: { (time: any): void; (arg0: any): void; }, wait: number | undefined) => {
+    let timeout: string | number | NodeJS.Timeout | undefined;
+    return function(...args: any) {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        // @ts-ignore
+        func(...args);
+      }, wait);
+    };
+  };
+
+  // Função de atualização do tempo restante com debounce
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const debouncedSetRemainingTime = useCallback(debounce((time) => {
+    setRemainingTime(time);
+  }, 500), []); // 500ms de debounce
 
   useEffect(() => {
     socket.on('timer-update', (time) => {
-      setRemainingTime(time);
+      debouncedSetRemainingTime(time);
     });
 
     return () => {
       socket.disconnect();
     };
-  }, []);
+  }, [debouncedSetRemainingTime, socket]);
 
   const startTimer = () => {
     const timerData = {
@@ -25,7 +43,7 @@ const Timer = () => {
     };
     socket.emit('start-timer', timerData); // Não há necessidade de JSON.stringify aqui
   };
-
+  console.log(remainingTime)
   return (
     <div>
       <h1>Timer</h1>
@@ -42,6 +60,7 @@ const Timer = () => {
       </div>
       <div>
         {remainingTime !== null && <h2>Remaining Time: {remainingTime} seconds</h2>}
+        <h2>Remaining Time: {remainingTime} seconds</h2>
       </div>
     </div>
   );
