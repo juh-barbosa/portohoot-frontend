531ccdef0b0422d3b901c0c2b15b8a566e49bb95
juh-barbosa <jujufreitas193@gmail.com>
2024-06-12 01:52:42 -0300

    FEAT - Frontend finished

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const http = axios.create({
-    baseURL: 'http://localhost:5000/api/'
+    baseURL: 'https://portohoot-backend.vercel.app/'
 });
 
 export default http;
